b5f78c931e558fb8e9d68342bc64a258c716b319
MuhammadRizkiRamdani <130382034+MuhammadRizkiRamdani@users.noreply.github.com>
2023-06-20 19:35:29 +0700

    Add files via upload

---
+++ Ekstrasi Fitur GLCM.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+from skimage.feature import graycomatrix, graycoprops
+from skimage import data
+PATCH_SIZE = 21
+
+image = data.camera()
+
+grass_locations = [(280, 454), (342, 223), (444, 192), (455, 455)]
+grass_patches = []
+for loc in grass_locations:
+    grass_patches.append(image[loc[0]:loc[0] + PATCH_SIZE,
+                               loc[1]:loc[1] + PATCH_SIZE])
+
+sky_locations = [(38, 34), (139, 28), (37, 437), (145, 379)]
+sky_patches = []
+for loc in sky_locations:
+    sky_patches.append(image[loc[0]:loc[0] + PATCH_SIZE,
+                             loc[1]:loc[1] + PATCH_SIZE])
+
+xs = []
+ys = []
+for patch in (grass_patches + sky_patches):
+    glcm = graycomatrix(patch, distances=[5], angles=[0], levels=256,
+                        symmetric=True, normed=True)
+    xs.append(graycoprops(glcm, 'dissimilarity')[0, 0])
+    ys.append(graycoprops(glcm, 'correlation')[0, 0])
+
+fig = plt.figure(figsize=(8, 8))
+
+ax = fig.add_subplot(3, 2, 1)
+ax.imshow(image, cmap=plt.cm.gray,
+          vmin=0, vmax=255)
+for (y, x) in grass_locations:
+    ax.plot(x + PATCH_SIZE / 2, y + PATCH_SIZE / 2, 'gs')
+for (y, x) in sky_locations:
+    ax.plot(x + PATCH_SIZE / 2, y + PATCH_SIZE / 2, 'bs')
+ax.set_xlabel('Original Image')
+ax.set_xticks([])
+ax.set_yticks([])
+ax.axis('image')
+
+ax = fig.add_subplot(3, 2, 2)
+ax.plot(xs[:len(grass_patches)], ys[:len(grass_patches)], 'go',
+        label='Grass')
+ax.plot(xs[len(grass_patches):], ys[len(grass_patches):], 'bo',
+        label='Sky')
+ax.set_xlabel('GLCM Dissimilarity')
+ax.set_ylabel('GLCM Correlation')
+ax.legend()
+
+for i, patch in enumerate(grass_patches):
+    ax = fig.add_subplot(3, len(grass_patches), len(grass_patches)*1 + i + 1)
+    ax.imshow(patch, cmap=plt.cm.gray,
+              vmin=0, vmax=255)
+    ax.set_xlabel('Grass %d' % (i + 1))
+
+for i, patch in enumerate(sky_patches):
+    ax = fig.add_subplot(3, len(sky_patches), len(sky_patches)*2 + i + 1)
+    ax.imshow(patch, cmap=plt.cm.gray,
+              vmin=0, vmax=255)
+    ax.set_xlabel('Sky %d' % (i + 1))
+
+fig.suptitle('Grey level co-occurrence matrix features', fontsize=14, y=1.05)
+plt.tight_layout()
+plt.show()
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+filename = 'checkerboard.png'
+img = cv.imread(filename)
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+cv.imshow('dst',img)
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
+plt.show()
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('riram.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+corners = cv.goodFeaturesToTrack(gray,25,0.01,10)
+corners = np.intp(corners)
+for i in corners:
+    x,y = i.ravel()
+    cv.circle(img,(x,y),3,255,-1)
+plt.imshow(img),plt.show()
